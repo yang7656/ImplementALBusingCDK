87c246d411d4782260acba868045184ec330189c
yang7656 <55370158+yang7656@users.noreply.github.com>
2024-05-06 11:59:26 -0500

    ALB

--- cdk_stack.py
+++ cdk_stack.py
@@ -36,33 +36,41 @@ class CdkStack(Stack):
         InstanceRole = iam.Role(self, "InstanceSSM", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"))
         InstanceRole.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMManagedInstanceCore"))
 
-        # Selecting the subnets for instances and load balancer explicitly
-        subnet_selection1 = ec2.SubnetSelection(subnet_group_name="PublicSubnet1")
-        subnet_selection2 = ec2.SubnetSelection(subnet_group_name="PublicSubnet2")
-
+        # User data script
+        user_data_script = """
+                           #!/bin/bash
+                           yum update -y
+                           yum install -y git httpd php
+                           service httpd start
+                           chkconfig httpd on
+                           aws s3 cp s3://seis665-public/index.php /var/www/html/
+                           """
+        user_data = ec2.UserData.for_linux()
+        user_data.add_commands(user_data_script)
+        
         instance1 = ec2.Instance(self, "web1",
                                  instance_type=ec2.InstanceType("t2.micro"),
                                  machine_image=ec2.AmazonLinuxImage(generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2),
                                  vpc=vpc,
-                                 vpc_subnets=subnet_selection1,
+                                 vpc_subnets=ec2.SubnetSelection(subnet_group_name="PublicSubnet1"),
                                  security_group=sg,
-                                 role=InstanceRole)
-
+                                 role=InstanceRole,
+                                 user_data=user_data)
+                                 
         instance2 = ec2.Instance(self, "web2",
                                  instance_type=ec2.InstanceType("t2.micro"),
                                  machine_image=ec2.AmazonLinuxImage(generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2),
                                  vpc=vpc,
-                                 vpc_subnets=subnet_selection2,
+                                 vpc_subnets=ec2.SubnetSelection(subnet_group_name="PublicSubnet2"),
                                  security_group=sg,
-                                 role=InstanceRole)
+                                 role=InstanceRole,
+                                 user_data=user_data)
 
         # Application Load Balancer
         lb = elbv2.ApplicationLoadBalancer(self, "EngineeringLB",
                                            vpc=vpc,
                                            internet_facing=True,
-                                           load_balancer_name="EngineeringLB",
-                                           vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC)
-                                          )
+                                           load_balancer_name="EngineeringLB")
                                            
         listener = lb.add_listener("Listener", port=80, open=True)
         listener.add_targets("Target",
@@ -71,4 +79,4 @@ class CdkStack(Stack):
                                       elbv2_targets.InstanceTarget(instance2, port=80)])
         
         # Output
-        CfnOutput(self, "LoadBalancerDNS", value=lb.load_balancer_dns_name)
+        CfnOutput(self, "WebUrl", value=lb.load_balancer_dns_name)
