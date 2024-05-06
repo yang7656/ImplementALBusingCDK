d795ab40daeadfd22bbddc807c121e2cad7067fc
yang7656 <55370158+yang7656@users.noreply.github.com>
2024-05-06 13:18:25 -0500

    alb

--- cdk_stack.py
+++ cdk_stack.py
@@ -70,8 +70,12 @@ class CdkStack(Stack):
         lb = elbv2.ApplicationLoadBalancer(self, "EngineeringLB",
                                            vpc=vpc,
                                            internet_facing=True,
-                                           load_balancer_name="EngineeringLB"
-                                          )
+                                           load_balancer_name="EngineeringLB",
+                                           vpc_subnets=ec2.SubnetSelection(
+                                               subnets=[
+                                                   vpc.public_subnets[0],
+                                                   vpc.public_subnets[1]
+                                               ]))
                                            
         listener = lb.add_listener("Listener", port=80, open=True)
         listener.add_targets("Target",
